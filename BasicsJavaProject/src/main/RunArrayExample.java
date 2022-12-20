6c8a349506c10357edb8ef52d468347da9f488af
stannarshosay <stannarsjose@gmail.com>
2022-12-20 11:29:32 +0530

    array example using function => input and output are arrays

--- BasicsJavaProject/src/main/RunArrayExample.java
+++ BasicsJavaProject/src/main/RunArrayExample.java
@@ -3,6 +3,31 @@ package main;
 import java.util.Scanner;
 
 public class RunArrayExample {
+	
+	
+	//the function is for storing data in a array	
+	// function definition
+	public static int[] populateArray(int numbers[]) {
+		
+		System.out.println("Enter numbers into array\n\n");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		for (int i = 0; i < numbers.length; i++) {
+			
+			System.out.println("Enter value for position => "+(i+1)+" of the array :");
+			
+			numbers[i] = scanner.nextInt();
+			
+		}
+		
+		//returning an array
+		return numbers;	
+				
+	}
+	
+	
+	
 
 	public static void main(String[] args) {
 		
@@ -16,12 +41,15 @@ public class RunArrayExample {
 		
 		numbers = new int[length];
 		
-		System.out.println("Enter numbers into array");
+		//function call
+		numbers = RunArrayExample.populateArray(numbers);	
 		
-		for (int i = 0; i < numbers.length; i++) {
-			
-			numbers[i] = scanner.nextInt();
-					
+		System.out.println("The Array is : ");
+		
+        for (int i = 0; i < numbers.length; i++) {
+        	
+        	System.out.print(numbers[i]+" ");
+								
 		}
 		
 
